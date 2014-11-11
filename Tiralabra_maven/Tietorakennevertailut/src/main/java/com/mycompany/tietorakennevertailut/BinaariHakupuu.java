335b7a543f2e49297b86581eb7a60eee99897497
tpheiska <tomi.heiskanen@helsinki.fi>
2014-11-11 15:24:17 +0200

    koodia gittiin

---
+++ Tiralabra_maven/Tietorakennevertailut/src/main/java/com/mycompany/tietorakennevertailut/BinaariHakupuu.java
@@ -0,0 +1,170 @@
+package com.mycompany.tietorakennevertailut;
+
+/**
+ *
+ * @author Tomi
+ * @version 1.0
+ * @since 6.11.2014
+ */
+public class BinaariHakupuu {
+    
+    private Solmu juuri;
+    
+    /**
+     * Muodostetaan ensin tyhjä hakupuu. Tämän jälkeen puuhun voi lisätä solmuja.
+     * Ensimmäinen solmu on juuri.
+     */
+    public BinaariHakupuu() {
+        juuri = null;
+    }
+    
+    /**
+     * Lisätään uusi arvo puuhun.
+     * @param arvo lisättävä arvo
+     */
+    public void lisaa(int arvo) {
+        
+        Solmu uusi = new Solmu(arvo);
+        if(juuri == null) {
+            juuri = uusi;
+        } else {
+            Solmu solmu = juuri, vanhempi = juuri;
+            while(solmu != null) {
+                vanhempi = solmu;
+                if(uusi.tieto() < solmu.tieto())
+                    solmu = solmu.vasen();
+                else
+                    solmu = solmu.oikea();
+            }
+            uusi.asetaVanhempi(vanhempi);
+            if(uusi.tieto() < vanhempi.tieto())
+                vanhempi.asetaVasen(uusi);
+            else
+                vanhempi.asetaOikea(uusi);
+        }
+        
+    }
+    
+    /**
+     * Tulostetaan puun solmujen tietojen arvot suuruusjärjestyksessä.
+     * @param solmu puun T alkiot voidaan tulostaa kutsumalla rekursiivista 
+     * algoritmia parametrilla T.juuri
+     */
+    public void tulosta(Solmu solmu) {
+        
+        if(solmu != null) {
+            tulosta(solmu.vasen());
+            System.out.println(solmu.tieto());
+            tulosta(solmu.oikea());
+        }
+    }
+    
+    /**
+     * Poistetaan arvoa vastaava solmu
+     * @param arvo poistettava arvo
+     */
+    public Solmu poista(int arvo) {
+        
+        Solmu pois = etsi(juuri, arvo), vanhempi=pois.vanhempi();
+        if(pois.vasen() == null && pois.oikea() == null) {
+            if(vanhempi.oikea().equals(pois))
+                vanhempi.asetaOikea(null);
+            else
+                vanhempi.asetaVasen(null);
+            return pois;
+        }
+        if(pois.vasen() == null || pois.oikea() == null) {
+            if(pois.tieto() < vanhempi.tieto())
+                if(pois.vasen() == null)
+                    vanhempi.asetaVasen(pois.oikea());
+                else
+                    vanhempi.asetaVasen(pois.vasen());
+            else
+                vanhempi.asetaOikea(pois);
+            return pois;
+        }
+        
+    }
+    
+    /**
+     * Etsitään haluttua arvoa.
+     * @param arvo arvo, jota etsitään
+     * @return palauttaa true, jos arvo löytyy puusta ja false, jos ei
+     */
+    public boolean etsi(int arvo) {
+        
+        return etsi(juuri, arvo) != null;
+    }
+    
+    /**
+     * Etsitään alipuusta arvoa rekursiivisesti
+     * @param solmu alipuun juuri solmu
+     * @param arvo etsitty arvo
+     * @return solmun, jonka tieto-kenttä vastaa arvoa tai null, jos arvoa ei
+     * löydy puusta
+     */
+    private Solmu etsi(Solmu solmu, int arvo) {
+        
+        if(solmu == null)
+            return null;
+        if(arvo < solmu.tieto())
+            return etsi(solmu.vasen(), arvo);
+        if(arvo > solmu.tieto())
+            return etsi(solmu.oikea(), arvo);
+        return solmu;
+    }
+    
+    /**
+     * Etsitään pienin arvo
+     * @return pienin arvo tai 0, jos tyhjä puu
+     */
+    public int etsiMin() {
+        
+        Solmu pienin = etsiMin(juuri);
+        if(pienin == null)
+            return 0;
+        return pienin.tieto();
+    }
+    
+    /**
+     * Etsitään alipuun pienin arvo
+     * @param juuri alipuun juuri
+     * @return viite pienimpään solmuun
+     */
+    private Solmu etsiMin(Solmu juuri) {
+        
+        if(juuri != null) {
+            while(juuri.vasen() != null)
+                juuri = juuri.vasen();
+        }
+        return juuri;
+    }
+    
+    /**
+     * Etsitään puun suurin arvo.
+     * @return suurin arvo tai 0, jos tyhjä puu
+     */
+    public int etsiMax() {
+        
+        Solmu suurin = etsiMax(juuri);
+        if(suurin == null)
+            return 0;
+        
+        return suurin.tieto();
+    }
+    
+    /**
+     * Etsitään alipuun suurin arvo
+     * @param juuri alipuun juuri
+     * @return viite suurimpaan solmuun
+     */
+    private Solmu etsiMax(Solmu juuri) {
+        
+        if(juuri != null) {
+            while(juuri.oikea() != null)
+                juuri = juuri.oikea();
+        }
+        return juuri;
+    }
+    
+}
---
+++ Tiralabra_maven/Tietorakennevertailut/src/main/java/com/mycompany/tietorakennevertailut/Solmu.java
@@ -0,0 +1,107 @@
+package com.mycompany.tietorakennevertailut;
+
+/**
+ *
+ * @author Tomi Heiskanen
+ * @version 1.0
+ * @since 6.11.2014
+ */
+public class Solmu {
+    
+    /**    
+     *Puiden tiedot ovat kokonaislukuja. Tieto voisi olla myös joku muu
+     * vertailtava arvo.
+     */
+    private int tieto;
+    
+    /**
+     * Viite vasempaan lapseen.
+     */
+    private Solmu vasen;
+    
+    /**
+     * Viite oikeaan lapseen
+     */
+    private Solmu oikea;
+    
+    /**
+     * Viite vanhempaan
+     */
+    private Solmu vanhempi;
+    
+    /**
+     * Alustetaan solmu.
+     * @param tieto toimii samalla solmun avaimena
+     */
+    public Solmu(int tieto) {
+        
+        this.tieto = tieto;
+    }
+    
+    /**
+     * @return palauttaa solmun tieto-kentän sisällön 
+     */
+    public int tieto() {
+        
+        return this.tieto;
+    }
+    
+    /**
+     * 
+     * @return palauttaa viitteen solmun vasempaan lapseen
+     */
+    public Solmu vasen() {
+        
+        return this.vasen;
+    }
+    
+    /**
+     * 
+     * @return palauttaa viitteen solmun oikeaan lapseen
+     */
+    public Solmu oikea() {
+        
+        return this.oikea;
+    }
+    
+    /**
+     * 
+     * @return palauttaa viitteen solmun vanhempaan
+     */
+    public Solmu vanhempi() {
+        
+        return this.vanhempi;
+    }
+    
+    /**
+     * Muuttaa tieto-kentän arvoa.
+     * @param uusiLuku tieto-kentän uusi arvo
+     */
+    public void muutaTietoa(int uusiLuku) {
+        
+        this.tieto = uusiLuku;
+    }
+    
+    /**
+     * Asettaa vasemman solmun viitteen
+     * @param vasen viite vasempaan solmuun
+     */
+    public void asetaVasen(Solmu vasen) {
+        
+        this.vasen = vasen;
+    }
+    
+    /**
+     * Asettaa oikean solmun viitteen
+     * @param oikea viite oikeaan solmuun
+     */
+    public void asetaOikea(Solmu oikea) {
+        
+        this.oikea = oikea;
+    }
+    
+    public void asetaVanhempi(Solmu vanhempi) {
+        
+        this.vanhempi = vanhempi;
+    }
+}
